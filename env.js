cc337a782d414179bc764149b14f5166d7656eaf
lsh <sermyes@gmail.com>
2021-02-12 02:44:24 +0900

    add env

---
+++ env.js
@@ -0,0 +1,5 @@
+export const Process = {
+    ENV_API_KEY = "AIzaSyD9FrJ-bsNIIkl6ld52vucKIEoe2QzLJvo",
+    ENV_AUTH_DOMAIN = "vegetable-game.firebaseapp.com",
+    ENV_PROJECT_ID = "vegetable-game",
+}
